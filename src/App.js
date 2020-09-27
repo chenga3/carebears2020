75ff9e33f3ee8b1831009fef09b955c6577986ef
“deborahlam123” <deborahlam123@gmail.com>
2020-09-27 08:15:58 +0800

    Merge Alan's CSS

--- src/App.js
+++ src/App.js
@@ -68,51 +68,60 @@ class PatientForm extends React.Component {
     const { showDischargeOptions } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <h1> Patient Form </h1>
-        <span>Is there any discharge:</span>
-        <input type="radio" id="discharge-yes" name="discharge" value="yes"
+        <h1 className="formheader"> Patient Form </h1>
+        <table>
+        <tr>
+        <td><span>Is there any discharge:</span></td>
+        <td><input type="radio" id="discharge-yes" name="discharge" value="yes"
           onChange={(event) => {
             this.setState({ showDischargeOptions: true });
             this.handleInputChange(event);
-          }}/> Yes
-        <input type="radio" id="discharge-no" name="discharge" value="no"
+          }}/> Yes </td>
+        <td><input type="radio" id="discharge-no" name="discharge" value="no"
           onChange={(event) => {
             this.setState({ showDischargeOptions: false });
             this.handleInputChange(event);
-          }}/> No
-        <br/>
+          }}/> No </td>
+        </tr>
         { showDischargeOptions ?
           <div className="discharge-options">
-            <span>Discharge colour:</span>
-            <input type="radio" name="dischargeColour" value="yellow"
-              onChange={this.handleInputChange}/> Yellow
-            <input type="radio" name="dischargeColour" value="green"
-              onChange={this.handleInputChange}/> Green
-            <br/>
-            <span>Does discharge have smelly odour:</span>
-            <input type="radio" id="discharge-odour" name="dischargeOdour" value="yes"
-              onChange={this.handleInputChange}/> Yes
-            <input type="radio" id="discharge-odour" name="dischargeOdour" value="no"
-              onChange={this.handleInputChange}/> No
-            <br/>
+            <table>
+            <tr>
+            <td><span>Discharge colour:</span></td>
+            <td><input type="radio" name="dischargeColour" value="yellow"
+              onChange={this.handleInputChange}/> Yellow </td>
+            <td><input type="radio" name="dischargeColour" value="green"
+              onChange={this.handleInputChange}/> Green </td>
+            </tr>
+            <tr>
+            <td><span>Does discharge have smelly odour:</span></td>
+            <td><input type="radio" id="discharge-odour" name="dischargeOdour" value="yes"
+              onChange={this.handleInputChange}/> Yes </td>
+            <td><input type="radio" id="discharge-odour" name="dischargeOdour" value="no"
+              onChange={this.handleInputChange}/> No </td>
+            </tr>
+            </table>
           </div> : null
         }
-        <span>Is there increasing pain:</span>
-        <input type="radio" id="pain" name="increasingPain" value="yes"
-          onChange={this.handleInputChange}/> Yes
-        <input type="radio" id="pain" name="increasingPain" value="no"
-          onChange={this.handleInputChange}/> No
-        <br/>
-        <span>Do you feel ill or have a fever:</span>
-        <input type="radio" id="ill" name="ill" value="yes"
-          onChange={this.handleInputChange}/> Yes
-        <input type="radio" id="ill" name="ill" value="no"
-          onChange={this.handleInputChange}/> No
-        <br/>
+        <tr>
+        <td><span>Is there increasing pain:</span></td>
+        <td><input type="radio" id="pain" name="increasingPain" value="yes"
+          onChange={this.handleInputChange}/> Yes </td>
+        <td><input type="radio" id="pain" name="increasingPain" value="no"
+          onChange={this.handleInputChange}/> No </td>
+        </tr>
+        <tr>
+        <td><span>Do you feel ill or have a fever:</span></td>
+        <td><input type="radio" id="ill" name="ill" value="yes"
+          onChange={this.handleInputChange}/> Yes</td>
+        <td><input type="radio" id="ill" name="ill" value="no"
+          onChange={this.handleInputChange}/> No </td>
+        </tr>
+        </table>
         <span>Extra comments:</span><br/>
         <textarea name="extraComments"
           onChange={this.handleInputChange}></textarea><br/>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" id="formsubmit"/>
       </form>
     );
   }
@@ -122,7 +131,7 @@ function App() {
   const [messages, setMessages] = useState([]);
   const [patients, setPatients] = useState([]);
   const [messageBody, setMessageBody] = useState('');
-  const patient = false;
+  const patient = true;
 
   useEffect(() => {
     API
@@ -249,25 +258,24 @@ function App() {
       <div className="messages">
         <div className="messages-scroller">
           {messages.map((message) => (
-            <div
-              key={message.id}
-              className={message.author === 'Dave' ? 'message me' : 'message'}>{message.body}</div>
+              <div
+                key={message.id}
+                className={message.author === 'Monica' ? 'message me' : 'message'}>{message.body}</div>
           ))}
         </div>
       </div>
       {patient === true ?
         <div className="submitoptions">
-        <Popup trigger={<button> Fill Form </button>} modal nested>
-          { close => (
-            <div className="modal">
-              <button className="close" onClick={close}> &times; </button>
-              <PatientForm/>
-            </div>
-          )}
-        </Popup>
-        <input type="file" id="file"/>
-        <button onClick={uploadImage}>Send Image</button>
-      </div>
+          <Popup trigger={<button className="submitoption" id="fillform"> Fill Form </button>} modal nested>
+            { close => (
+              <div className="modal">
+                <button className="close" onClick={close}> &times; </button>
+                <PatientForm/>
+              </div>
+            )}
+          </Popup>
+          <button onClick={uploadImage} className="submitoption" id="sendimage">Send Image</button>
+        </div>
       :<div>
           <div className="responseOptions">
           <form onSubmit={handleSubmit}>
